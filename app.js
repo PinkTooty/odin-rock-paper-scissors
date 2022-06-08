4b9efc4108b2c5691c8e14d7d43385ea5277edb0
PinkToot <huynh.daithanh1@gmail.com>
2022-06-07 20:43:10 -0400

    Add first version of interactive UI for RPS game

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-let computerInput;
+const buttons = document.querySelectorAll('.selections')
+const rk = document.querySelector('#rock')
+const ppr = document.querySelector('#paper')
+const scr = document.querySelector('#scissor')
+const result = document.querySelector('.result')
+const score = document.querySelector('.score')
+const play = document.querySelector('.play')
+const reset = document.querySelector('.reset')
+
+let playerInput;
 
 let p1Score = 0;
 let p2Score = 0;
@@ -7,29 +16,22 @@ const rock = 'rock';
 const paper = 'paper';
 const scissor = 'scissor';
 
-const p1Win = "Player wins!";
-const p2Win = "Computer wins!";
+const p1Win = "Player won!";
+const p2Win = "Computer won!";
 const tie = "Tie!";
 
-function getRandomInt() {
-    return Math.floor(Math.random() * (3) + 1);
-}
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerInput = button.getAttribute('id')
+    })
+})
 
 function playerPlay() {
-    let playerInput = prompt("Please enter either rock, paper, or scissors").toLowerCase();
-    if (playerInput === 'scissors') {
-        return playerInput = scissor;
-    }
-    else if (playerInput === '' || playerInput !== rock && playerInput !== paper && playerInput !== scissor) {
-        throw 'Please try again!';
-    }
-    else {
-        return playerInput;
-    }
+    return playerInput;
 }
 
 function computerPlay() {
-    computerInput = getRandomInt();
+    computerInput = Math.floor(Math.random() * (3) + 1);
     if (computerInput === 1) {
         return rock;
     }
@@ -40,47 +42,82 @@ function computerPlay() {
         return scissor;
     }
 }
+
+function scoreReset() {
+    p1Score = 0;
+    p2Score = 0;
+    result.textContent = null
+    score.textContent = `Player Score: ${p1Score} Computer Score: ${p2Score}`
+}
+
+function addResult(winner) {
+    result.textContent = winner;
+}
+
+function updateScore() {
+    score.textContent = `Player Score: ${p1Score} Computer Score: ${p2Score}`
+    if (p1Score == 5) {
+        score.textContent = `Player is the winner!`
+        play.textContent = 'Play more?'
+    }
+    if (p2Score == 5) {
+        score.textContent = 'Computer is the winner!'
+        play.textContent = 'Play more?'
+    }
+}
+
 // plays a single round
 function playRound() {
     let computerSelection = computerPlay();
     let playerSelection = playerPlay();
+    if (!playerSelection) {
+        result.textContent = 'Please choose an option'
+        return
+    }
     if (playerSelection === rock && computerSelection === scissor) {
         p1Score++;
-        return p1Win;
+        addResult(p1Win)
     } else if (playerSelection === rock && computerSelection === paper) {
         p2Score++;
-        return p2Win;
-    }
-    if (playerSelection === paper && computerSelection === rock) {
+        addResult(p2Win);
+    } else if (playerSelection === paper && computerSelection === rock) {
         p1Score++;
-        return p1Win;
+        addResult(p1Win);
     } else if (playerSelection === paper && computerSelection === scissor) {
         p2Score++;
-        return p2Win;
-    }
-    if (playerSelection === scissor && computerSelection === paper) {
+        addResult(p2Win);
+    } else if (playerSelection === scissor && computerSelection === paper) {
         p1Score++;
-        return p1Win;
+        addResult(p1Win);
     } else if (playerSelection === scissor && computerSelection === rock) {
         p2Score++;
-        return p2Win;
-    }
-    else {
-        return tie;
+        addResult(p2Win);
+    } else if (playerSelection === computerSelection) {
+        addResult(tie);
     }
 }
+
+play.addEventListener('click', () => {
+    playRound()
+    updateScore()
+})
+
+reset.addEventListener('click', () => {
+    scoreReset()
+})
+
 // plays 5 rounds
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    if (p1Score > p2Score) {
-        return p1Win;
-    }
-    else if (p1Score < p2Score) {
-        return p2Win;
-    }
-    else {
-        return tie;
-    }
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         playRound();
+//     }
+//     if (p1Score > p2Score) {
+//         return p1Win;
+//     }
+//     else if (p1Score < p2Score) {
+//         return p2Win;
+//     }
+//     else {
+//         return tie;
+//     }
+// }
